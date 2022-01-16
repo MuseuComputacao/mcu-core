8e647c1a5b18c588766690f1bf83484d35a74306
Fábio R. Nóbrega <fabio.r.nobrega@gmail.com>
2022-01-16 18:24:27 -0300

    feat(config/initializers/devise_token_auth.rb): test devise routers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require Rails.root.join('lib', 'api_constraints')
 
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
   namespace :api, defaults: { format: :json } do
     mount_devise_token_auth_for 'User', at: 'auth'
     scope module: :v1,
