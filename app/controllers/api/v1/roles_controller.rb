b9105571490c4daab8d29b143b56f5a7d1eb6d69
Fábio R. Nóbrega <fabio.r.nobrega@gmail.com>
2022-01-23 11:26:12 -0300

    feat(app/controllers/api/v1/roles_controller.rb): only list roles when user has master role

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -4,7 +4,11 @@ class Api::V1::RolesController < ApplicationController
   before_action :authenticate_api_user!
 
   def index
+    if current_api_user[:role] == 'master' 
     roles = %w[master cataloger editor curator]
     render(json: roles.to_json)
+    else 
+      render(json: { message: 'Unaurthorized role' }, status: 401)
+    end
   end
 end
