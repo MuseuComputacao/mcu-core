0f3c7cb88b2acbe8149bce8ad76942779a8cb3b9
Fábio R. Nóbrega <fabio.r.nobrega@gmail.com>
2022-01-23 10:52:12 -0300

    feat(app/controllers/api/v1/roles_controller.rb): autentication roles index router

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Api::V1::RolesController < ApplicationController
+  before_action :authenticate_api_user!
+
   def index
     roles = %w[master cataloger editor curator]
     render(json: roles.to_json)
