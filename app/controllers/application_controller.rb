7ded91521c09b30f999ac0feef4775f8f89521d8
Fábio R. Nóbrega <fabio.r.nobrega@gmail.com>
2022-09-29 19:27:45 -0300

    fix(app/controllers/application_controller.rb): allow update first login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name role])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name role first_login])
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name role])
   end
 end
